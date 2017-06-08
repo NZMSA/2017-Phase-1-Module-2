cac76a0f6cc3c01460d0073dd368642cc7f79b16
Jason Goodsell <jason.goodsell@studentpartner.com>
2017-06-08 17:17:39 +1200

    Added the ability to take photos and support for custom vision API.

--- 4. Integrating Camera into Xamarin app/CameraCompleted/Tabs/CustomVision.xaml.cs
+++ 4. Integrating Camera into Xamarin app/CameraCompleted/Tabs/CustomVision.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Tabs
@@ -10,6 +15,63 @@ namespace Tabs
         public CustomVision()
         {
             InitializeComponent();
+        }
+
+        private async void loadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = String.Format("{0}.jpg", DateTime.Now.Date.ToString())
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+
+            await MakePredictionRequest(file);
+        }
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadBytes((int)stream.Length);
+        }
+
+        static async Task MakePredictionRequest(MediaFile file)
+        {
+            var client = new HttpClient();
+            
+            client.DefaultRequestHeaders.Add("Prediction-Key", "a51ac8a57d4e4345ab0a48947a4a90ac");
+            
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/4da1555c-14ca-4aaf-af01-d6e1e97e5fa6/image?iterationId=7bc76035-3825-4643-917e-98f9d9f79b71";
+
+            HttpResponseMessage response;
+            
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                Debug.WriteLine(await response.Content.ReadAsStringAsync());
+            }
         }
     }
 }
