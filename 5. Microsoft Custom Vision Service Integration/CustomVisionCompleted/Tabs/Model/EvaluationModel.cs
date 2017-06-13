5b51874f540528cc224fd54fab90289e7c0c2ea9
Jason Goodsell <jasong@datacom.co.nz>
2017-06-13 19:33:34 +1200

    Added evaluation model used to deserialise JSON response.

---
+++ 5. Microsoft Custom Vision Service Integration/CustomVisionCompleted/Tabs/Model/EvaluationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tabs.Model
+{
+    public class EvaluationModel
+    {
+		public string Id { get; set; }
+		public string Project { get; set; }
+		public string Iteration { get; set; }
+		public string Created { get; set; }
+		public List<Prediction> Predictions { get; set; }
+    }
+
+	public class Prediction
+	{
+		public string TagId { get; set; }
+		public string Tag { get; set; }
+		public double Probability { get; set; }
+	}
+}
